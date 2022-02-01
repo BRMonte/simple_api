0f4bec4002093174686e72912e916bcb93c47edc
Bruno Montebello <brunormontebello@hotmail.com>
2022-01-31 23:44:16 -0300

    Refact controller by creating a concern for api response

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -1,24 +1,14 @@
 class Api::V1::RegistrationsController < Devise::RegistrationsController
-
   before_action :ensure_params_exist, only: :create
+
   #sign_up
   def create
     user = User.new(user_params)
 
     if user.save
-      render json: {
-        messages: "Signed up successfully",
-        is_success: true,
-        data: {
-          user: user
-        }
-      }, status: :ok
+      json_response("Signed up successfully", true, {user: user}, :ok)
     else
-      render json: {
-        messages: "Something went wrong",
-        is_success: false,
-        data: {},
-      }, status: :unprocessable_entity
+      json_response("Something went wrong", false, {}, :unprocessable_entity)
     end
   end
 
@@ -29,11 +19,7 @@ class Api::V1::RegistrationsController < Devise::RegistrationsController
 
   def ensure_params_exist
     return if params[:user].present?
-    render json: {
-      messages: "Missing params",
-      is_success: false,
-      data: {}
-    }, status: :bad_request
+    json_response("Missing params", false, {}, :bad_request)
   end
 
 end
